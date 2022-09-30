721efbca2acd6d10ffefd5c23d442e7053eeb1ce
K <k.rev.urtica@gmail.com>
2022-09-30 14:49:30 +0900

    ✨ feat: add github repo link

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,45 +1,39 @@
 import { EditWeight } from './EditWeight/EditWeight'
 import { EditColor } from './EditColor/EditColor'
 import { PreviewSettings } from './PreviewSettings/PreviewSettings'
-import { AppLink } from '../Common/AppLink'
+import { ScrollArea } from '@mantine/core'
+import { SidePanelActions } from './SidePanelActions/SidePanelActions'
 
 const SidePanel = () => {
   return (
-    <nav className="flex h-screen w-full flex-col gap-6 p-5 md:w-80 lg:w-96">
-      <div>
-        <h1 className="text-center text-2xl font-black text-zinc-100">
-          Tints and Shades
-          <br />
-          Generator
-        </h1>
-      </div>
-
-      <div className="flex-1">
-        <div className="flex flex-col gap-4">
-          <section className="border-t border-zinc-600/60 pt-4">
-            <EditColor />
-          </section>
-          <section>
-            <EditWeight />
-          </section>
-
-          <section className="border-t border-zinc-600/60 pt-4">
-            <PreviewSettings />
-          </section>
+    <nav className="flex h-screen w-full flex-col border-none border-zinc-700 md:w-80 md:border-r md:border-solid lg:w-96">
+      <ScrollArea offsetScrollbars className="flex-1 px-5">
+        <div className="py-4">
+          <h1 className="text-center text-2xl font-black text-zinc-100">
+            Tints and Shades
+            <br />
+            Generator
+          </h1>
         </div>
-      </div>
 
-      <div>
-        <div className="text-center text-zinc-400">
-          Made by{' '}
-          <AppLink
-            to="https://twitter.com/k_urtica"
-            className="text-emerald-300"
-          >
-            K(k_urtica)
-          </AppLink>{' '}
-          💚 with Gatsby
+        <div>
+          <div className="flex flex-col gap-4">
+            <section className="border-t border-zinc-600/60 pt-4">
+              <EditColor />
+            </section>
+            <section>
+              <EditWeight />
+            </section>
+
+            <section className="border-t border-zinc-600/60 pt-4">
+              <PreviewSettings />
+            </section>
+          </div>
         </div>
+      </ScrollArea>
+
+      <div className="border-t border-zinc-700">
+        <SidePanelActions />
       </div>
     </nav>
   )
