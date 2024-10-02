fc8dc71ad9227944ed8a2db95389db18265bc819
K <k.rev.urtica@gmail.com>
2024-10-02 20:54:53 +0900

    refactor

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,7 @@
 <template>
   <main class="bg-background">
-    <header
-      class="flex h-16 items-center justify-center border-b text-xl font-bold lg:hidden"
-    >
-      Tints and Shades Generator
+    <header class="flex h-16 items-center justify-center md:hidden">
+      <h1 class="text-xl font-black">Tints and Shades Generator</h1>
     </header>
 
     <NuxtPage />
--- app/stores/appStore.ts
+++ app/stores/appStore.ts
@@ -1,15 +1,17 @@
+type Appearance = {
+  isPadded: boolean;
+  isOneLine: boolean;
+  indicator: boolean;
+  copyWithHash: boolean;
+  border: boolean;
+};
+
 export const useAppStore = defineStore('tints-and-shades-app', () => {
   const color = ref('#ff9ea5');
 
   const weight = ref(8);
 
-  const appearance = ref<{
-    isPadded: boolean;
-    isOneLine: boolean;
-    indicator: boolean;
-    copyWithHash: boolean;
-    border: boolean;
-  }>({
+  const appearance = ref<Appearance>({
     isPadded: true,
     isOneLine: false,
     indicator: true,
