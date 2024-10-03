fbd0462e573c8061ffd4648ec40c582ca4b4a26e
K <k.rev.urtica@gmail.com>
2024-10-03 20:40:58 +0900

    fix: text color

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="bg-background">
     <header class="flex h-16 items-center justify-center md:hidden">
-      <h1 class="text-xl font-black">Tints and Shades Generator</h1>
+      <h1 class="text-xl font-black dark:text-white">Tints and Shades Generator</h1>
     </header>
 
     <NuxtPage />
--- app/components/SidePanel.vue
+++ app/components/SidePanel.vue
@@ -5,7 +5,7 @@ const { color, weight } = storeToRefs(useAppStore());
 <template>
   <div class="sticky top-3 my-3 ml-3 h-[calc(100dvh-24px)] w-[340px]">
     <div class="flex h-full flex-col overflow-auto rounded-xl border bg-surface p-6">
-      <h1 class="text-balance text-center text-xl font-black">
+      <h1 class="text-balance text-center text-xl font-black dark:text-white">
         Tints and Shades Generator
       </h1>
       <div class="mt-12 flex flex-col gap-8 pb-10">
