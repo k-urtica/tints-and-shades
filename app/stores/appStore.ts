0941826a6174ea1836bca0193b21baa9150946cb
K <k.rev.urtica@gmail.com>
2024-10-03 20:00:54 +0900

    fix: update default values

--- app/stores/appStore.ts
+++ app/stores/appStore.ts
@@ -7,16 +7,16 @@ type Appearance = {
 };
 
 export const useAppStore = defineStore('tints-and-shades-app', () => {
-  const color = ref('#ff9ea5');
+  const color = ref('#41339c');
 
-  const weight = ref(8);
+  const weight = ref(6);
 
   const appearance = ref<Appearance>({
     isPadded: true,
     isOneLine: false,
     indicator: true,
     copyWithHash: true,
-    border: false,
+    border: true,
   });
 
   return {
