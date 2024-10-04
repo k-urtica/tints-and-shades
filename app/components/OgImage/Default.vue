c18e2da2598ef35fdf0a6605a95c48808612caa7
K <k.rev.urtica@gmail.com>
2024-10-04 22:28:48 +0900

    feat: update OG image

--- app/components/OgImage/Default.vue
+++ app/components/OgImage/Default.vue
@@ -5,11 +5,16 @@ defineProps<{
 </script>
 
 <template>
-  <div class="bg-[#121212] p-10">
+  <div
+    class="bg-[#121212] p-10"
+    :style="{
+      backgroundImage: `url('/ogBg.png')`,
+    }"
+  >
     <div
       class="flex h-full items-center justify-center rounded-3xl border border-zinc-700"
     >
-      <div class="text-6xl font-black text-white">
+      <div class="text-7xl font-black text-amber-400">
         {{ title }}
       </div>
     </div>
