aa74ac187bbfb70875e0af5bd49dba9f194d2155
K <k.rev.urtica@gmail.com>
2025-04-18 21:41:30 +0900

    fix: update Google Analytics ID

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     scripts: {
       registry: {
         googleAnalytics: {
-          id: 'G-T1QZY0VRVC',
+          id: 'G-FS97XXKP7P',
         },
         googleAdsense: {
           client: 'ca-pub-6155745382628091',
