e83aa51b0e28d7e27f529486cbd653c3d106ceb7
K <k.rev.urtica@gmail.com>
2025-04-20 10:23:54 +0900

    wip: enabled ads

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,10 +9,22 @@ useHead({
 </script>
 
 <template>
-  <div class="flex min-h-dvh flex-row gap-6">
+  <div class="flex min-h-dvh flex-row">
     <SidePanel class="hidden md:block" />
 
-    <Generator class="flex-1 border md:mt-3 md:rounded-tl-xl" />
+    <Generator class="flex-1 md:mt-3 md:ml-6 md:rounded-t-xl md:border" />
+
+    <div class="hidden w-[280px] shrink-0 p-4 xl:block">
+      <ScriptGoogleAdsense
+        data-ad-client="ca-pub-6155745382628091"
+        data-ad-slot="3784163880"
+        :data-full-width-responsive="false"
+        data-ad-format="auto"
+        class="sticky top-4 mx-auto max-w-full"
+      >
+        <template #error>Please disable your ad blocker.</template>
+      </ScriptGoogleAdsense>
+    </div>
 
     <MobileDrawer class="md:hidden" />
   </div>
