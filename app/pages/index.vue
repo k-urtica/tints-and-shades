7dc54c659566f83e215b3a8dcc4f27a32cf358fc
K <k.rev.urtica@gmail.com>
2025-07-27 00:11:00 +0900

    refactor: rename components

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,11 +8,11 @@ useHead({
 
 <template>
   <div class="flex h-[calc(100dvh-(var(--header-height)))] flex-1 flex-row">
-    <SidePanel class="hidden lg:block" />
+    <SettingsPanel class="hidden lg:block" />
 
-    <Generator class="flex-1" />
+    <ColorGenerator class="flex-1" />
 
-    <AdSidebar class="hidden shrink-0 xl:block" />
+    <SidebarAds class="hidden shrink-0 xl:block" />
 
     <MobileDrawer class="lg:hidden" />
   </div>
